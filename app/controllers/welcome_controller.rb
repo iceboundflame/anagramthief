c5cbcf34b02b7116918f1575677dc4c20adcd186
davidcyl <david@iceboundflame.com>
2011-05-11 01:52:37 -0700

    remove extraneous prints

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,7 @@ class WelcomeController < ApplicationController
   def index
     on_canvas = process_signed_request
     if user_signed_in?
-        puts "getting go_to_game_id to #{session[:go_to_game_id]}"
       game_id = params[:game_id] || session[:go_to_game_id]
-        puts "go_to_game_id is #{game_id}"
       if game_id
         session.delete :go_to_game_id
         redirect_to play_url(game_id)
@@ -13,10 +11,7 @@ class WelcomeController < ApplicationController
       end
     else
       # Render sign-in link
-      if params[:game_id]
-        session[:go_to_game_id] = params[:game_id]
-        puts "Setting go_to_game_id to #{session[:go_to_game_id]}"
-      end
+      session[:go_to_game_id] = params[:game_id] if params[:game_id]
       @guest_name = random_guest_name
     end
   end
